f9465d54f146b869e44d443aeceaf7ca8d1ea9db
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2021-10-02 14:14:13 +0100

    feat: add sample app

---
+++ Example/SpanGridSample/SpanGridSample/SpanGridSampleApp.swift
@@ -0,0 +1,16 @@
+//
+// SpanGridSampleApp.swift
+//
+// Copyright 2021 • James Sherlock
+//
+
+import SwiftUI
+
+@main
+struct SpanGridSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
